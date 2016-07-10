b01f919778c323b764cc21518fccdf46db7ed5c1
*kelvinmuchui* <muchuikelvin423@gmail.com>
2016-07-10 14:00:59 +0300

    modified app.py

--- app/app.py
+++ app/app.py
@@ -38,7 +38,18 @@ def appointment_detail(appointment_id):
 @app.route('/appointments/<int:appointment_id>/edit/',methods=['GET', 'POST'])
 #@app.route(...) and def appointment_edit(...).
 def appointment_edit(appointment_id):
- 	return 'Form to edit appointment #.'.format(appointment_id)
+	"""Provide HTML form to edit a given appointment."""
+	appt = db.session.query(Appointment).get(appointment_id)
+	if appt is None:
+		abort(404)
+	form = AppointmentForm(request.form, appt)
+	if request.method == 'POST' and form.validate():
+		form.populate_obj(appt)
+		db.session.commit()
+		# Success. Send the user back to the detail view.
+		return redirect(url_for('appointment_detail', appointment_id=appt.id))
+		return render_template('appointment/edit.html', form=form)
+
 
 @app.route('/appointments/create/', methods=['GET', 'POST'])
 def appointment_create():
@@ -64,6 +75,15 @@ def index():
 	# Either first load or validation error at this point.
 	return render_template('appointment/edit.html',
 	form=form)
-	
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+	"""Provide HTML page with a given appointment."""
+	# Query: get Appointment object by ID.
+	appt = db.session.query(Appointment).get(appointment_id)
+	if appt is None:
+		# Abort with Not Found.
+		abort(404)
+	return render_template('appointment/detail.html',
+	appt=appt)
 if __name__ == '__main__':
 	app.run(debug=True)
